25ed365fbb44bec5703e0b2f9c62104c38ad7382
youngjoong <2403110262@office.kopo.ac.kr>
2024-05-13 14:46:33 +0900

    dzdz

---
+++ src/BOJ/BOJ11728.java
@@ -0,0 +1,10 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class BOJ11728 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
--- src/BOJ/BOJ1541.java
+++ src/BOJ/BOJ1541.java
@@ -3,8 +3,32 @@ package BOJ;
 import java.util.Scanner;
 
 public class BOJ1541 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String adds[] = scanner.nextLine().split("-");
+
+        int ops[] = new int[adds.length];
+        for (int i = 0; i < adds.length; i++) {
+            if (adds[i].contains("+"))
+                ops[i] = evaluate(adds[i]);
+            else
+                ops[i] = Integer.parseInt(adds[i]);
+        }
+        int result = ops[0];
+
+        for (int i = 1; i < ops.length; i++)
+            result -= ops[i];
+
+        System.out.println(result);
+    }
+
+    static int evaluate(String inStr) {
+        String[] adds = inStr.split("\\+");
+        int sum = 0;
+
+        for (String add : adds)
+            sum += Integer.parseInt(add);
+        return sum;
     }
 }
